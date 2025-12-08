2568427e39db927aa460a7d8c13802d49639ea2c
Merk48 <meryKassis48@gmail.com>
2025-12-08 15:40:47 +0300

    feat: create configuration table model and set the prisma synch with it

--- db/index.ts
+++ db/index.ts
@@ -2,43 +2,21 @@ import { PrismaClient } from "@/lib/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
+  connectionString: process.env.DATABASE_URL,
 });
 
-declare global {
-  var cachedPrisma: PrismaClient;
-}
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    adapter,
+  });
+};
 
-let prisma: PrismaClient;
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient({ adapter });
-} else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient({ adapter });
-  }
-  prisma = global.cachedPrisma;
-}
+const db = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export const db = prisma;
+export default db;
 
-// import { PrismaClient } from "../lib/generated/prisma/client";
-// import { PrismaPg } from "@prisma/adapter-pg";
-
-// const globalForPrisma = global as unknown as {
-//   prisma: PrismaClient;
-// };
-
-// const adapter = new PrismaPg({
-//   connectionString: process.env.DATABASE_URL,
-// });
-
-// const prisma =
-//   globalForPrisma.prisma ||
-//   new PrismaClient({
-//     adapter,
-//   });
-
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-// export const db = prisma;
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = db;
